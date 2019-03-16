f9e08d18245ba0276cc493daae4e5f97f491dd33
J. Singleton <jennifer.singleton@gmail.com>
2019-03-16 20:03:36 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -14,14 +14,8 @@ class Pokemon
   end
 
   def self.find(id, db)
-    new_pokemon_id = db.execute("SELECT id FROM pokemon WHERE id = id;")
-    new_pokemon_name = db.execute("SELECT name FROM pokemon WHERE id = id;")
-    new_pokemon_type = db.execute("SELECT type FROM pokemon WHERE id = id;")
-
+    new_pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?; id").flatten
     new_pokemon = Pokemon.new(id:id, name:new_pokemon_name[0][0], type:new_pokemon_type[0][0], db:db)
-
-    
-    new_pokemon.hp = db.execute("SELECT hp FROM pokemon WHERE id = id;")
   end
 
   def alter_hp(hp, db)
