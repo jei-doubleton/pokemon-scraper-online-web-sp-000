a4214c57cc4bd9060c93fbe288fb3c6179a494a5
J. Singleton <jennifer.singleton@gmail.com>
2019-03-16 19:47:30 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -17,6 +17,7 @@ class Pokemon
     new_pokemon_id = db.execute("SELECT id FROM pokemon WHERE id = id;")
     new_pokemon_name = db.execute("SELECT name FROM pokemon WHERE id = id;")
     new_pokemon_type = db.execute("SELECT type FROM pokemon WHERE id = id;")
+    new_pokemon_hp = db.execute("SELECT")
 
     Pokemon.new(id:id, name:new_pokemon_name[0][0], type:new_pokemon_type[0][0], db:db)
   end
