59fc19cf4e383be6c9860cda219355294f31b183
J. Singleton <jennifer.singleton@gmail.com>
2019-03-16 19:23:41 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,7 +2,7 @@ require 'pry'
 class Pokemon
   attr_accessor :id, :name, :type, :db
 
-  def initialize(id:, name:, type:, db:, hp:nil)
+  def initialize(id:, name:, type:, hp:nil, db:)
     @id = id
     @name = name
     @type = type
@@ -19,7 +19,7 @@ class Pokemon
     new_pokemon_type = db.execute("SELECT type FROM pokemon WHERE id = id;")
     new_pokemon_hp = db.execute("SELECT hp FROM pokemon WHERE id = id;")
 
-    Pokemon.new(id:id, name:new_pokemon_name[0][0], type:new_pokemon_type[0][0], hp:new_pokemon_hp[0][0] db:db)
+    Pokemon.new(id:id, name:new_pokemon_name[0][0], type:new_pokemon_type[0][0], hp:new_pokemon_hp[0][0], db:db)
   end
 
 
