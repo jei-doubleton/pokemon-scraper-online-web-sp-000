b6827335b2b05a6c8a80537e7b00efbc04ad71ab
J. Singleton <jennifer.singleton@gmail.com>
2019-03-16 19:13:09 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,7 +15,8 @@ attr_accessor :id, :name, :type, :db
   def self.find(id, db)
     new_pokemon_id = db.execute("SELECT id FROM pokemon WHERE id = id;")
     new_pokemon_name = db.execute("SELECT name FROM pokemon WHERE id = id;")
-    new_pokemon_type = db.execute("SELECT type FROM pokemon WHERE id = id")
+    new_pokemon_type = db.execute("SELECT type FROM pokemon WHERE id = id;")
+    
   end
 
 end
